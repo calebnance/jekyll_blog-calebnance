4913f435b999aa39ca2ae2559b6a738a0896e1bf
Caleb Nance <calebnance@gmail.com>
2018-06-18 01:14:09 -0400

    turned pwa back on

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,31 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('cn_blog').then(cache => {
+var cacheName = 'cn-blog';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/bootstrap.min.css',
+  '/assets/css/main.css',
+  '/assets/js/jquery-3.1.1.min.js',
+  '/assets/js/bootstrap.min.js'
+];
 
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/assets/css/bootstrap.min.css',
-        '/assets/css/main.css',
-        '/assets/js/jquery-3.1.1.min.js',
-        '/assets/js/bootstrap.min.js',
-        '/assets/js/scripts.min.js'
-      ])
-      .then(() => self.skipWaiting());
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
     })
-  )
+  );
 });
 
 self.addEventListener('activate',  event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(function() {
-
-      return caches.match(event.request);
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
