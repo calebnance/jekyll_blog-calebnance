960f9197e07b8b56482a444b30ec01f9a11639e6
Caleb Nance <calebnance@gmail.com>
2018-07-18 15:53:55 -0400

    updating navbar, this could potentially speed the site up a lot

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,31 @@
-var cacheName = 'cn-blog';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/assets/css/bootstrap.min.css',
-  '/assets/css/main.css',
-  '/assets/js/jquery-3.1.1.min.js',
-  '/assets/js/bootstrap.min.js'
-];
-
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// var cacheName = 'cn-blog';
+// var filesToCache = [
+//   '/',
+//   '/index.html',
+//   '/assets/css/bootstrap.min.css',
+//   '/assets/css/main.css',
+//   '/assets/js/jquery-3.1.1.min.js',
+//   '/assets/js/bootstrap.min.js'
+// ];
+//
+// self.addEventListener('install', function(e) {
+//   console.log('[ServiceWorker] Install');
+//   e.waitUntil(
+//     caches.open(cacheName).then(function(cache) {
+//       console.log('[ServiceWorker] Caching app shell');
+//       return cache.addAll(filesToCache);
+//     })
+//   );
+// });
+//
+// self.addEventListener('activate',  event => {
+//   event.waitUntil(self.clients.claim());
+// });
+//
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request, {ignoreSearch:true}).then(response => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
