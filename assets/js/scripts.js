0186b9e5257b05e69b72cbeb6ea04ad313d35fde
Caleb Nance <calebnance@gmail.com>
2018-07-25 09:18:15 -0400

    fixed the load in comments

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,27 @@
-$(document).ready(function() {
-  // show disqus comments (only on user click)
-  $('.show-comments').on('click', function(){
-    var disqus_shortname = 'blogcalebnancecom';
+function getComments() {
+  var disqus_shortname = 'blogcalebnancecom';
+  var e = document.createElement("script");
+  e.type = "text/javascript";
+  e.async = true;
+  e.src = "//" + disqus_shortname + ".disqus.com/embed.js";
+  (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(e);
 
-    // ajax request to load the disqus javascript
-    $.ajax({
-      type: "GET",
-      url: "https://" + disqus_shortname + ".disqus.com/embed.js",
-      dataType: "script",
-      cache: true
-    });
+  // hide comment block
+  this.style.display = 'none';
+}
 
-    // hide the button once comments load
-    $(this).fadeOut();
-  });
-});
+function domLoaded() {
+  // grab comment block
+  var comments = document.getElementsByClassName('show-comments');
+  // do we have comment blocks?
+  if (comments.length) {
+    // add event listener
+    comments[0].addEventListener('click', getComments);
+  }
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", domLoaded);
+} else {
+  domLoaded();
+}
